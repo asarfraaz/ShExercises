ec22a2facc6d26ed319bf5da3091c7446bd582ec
Sarfraaz Ahmed <sarfraaz@gmail.com>
2016-02-08 11:45:10 +0530

    Template for Set 01, Q01
    This template contains the script to show the current date

---
+++ introduction/s01q01.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+say_hello(){
+    echo "Hello World"
+}
+
+show_date(){
+    # Write your solution commands here
+    echo
+}
+
+# Main program starts here
+say_hello
+show_date
